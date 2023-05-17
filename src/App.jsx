b9b5d911bab6601e8c4edd4da059e100aa4bc4b5
sadiawcutecat <98077710+sadiawcutecat@users.noreply.github.com>
2023-05-18 02:30:50 +0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Footer from './Component/Footer'
 import Navsection from './Component/Navsection'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
    <Navsection></Navsection>
+   <Footer></Footer>
     </>
   )
 }
