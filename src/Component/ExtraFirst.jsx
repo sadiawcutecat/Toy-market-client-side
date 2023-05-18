6b99dfcce6bfd26f8ddb7a8ba9e2b9c7035e4542
sadiawcutecat <98077710+sadiawcutecat@users.noreply.github.com>
2023-05-18 13:51:55 +0600

    extra section one modified

--- src/Component/ExtraFirst.jsx
+++ src/Component/ExtraFirst.jsx
@@ -2,44 +2,52 @@ import React from 'react';
 import choose from '../assets/images/choose.jpg'
 import plan from '../assets/images/plan.jpg'
 import swap from '../assets/images/swap.jpg'
+import { FaArrowCircleRight } from 'react-icons/fa';
 
 const ExtraFirst = () => {
     return (
 
 
         <div>
-            <h1 className='text-accent text-5xl text-center font-bold font-serif mt-8 '>How To Buy ?</h1>
-            <div className='flex justify-center gap-20'>
-                <div className='my-6'>
+            <h1 className='text-accent text-5xl text-center font-bold font-serif mt-8 mb-8'>How To Buy ?</h1>
+            <div className='flex justify-center gap-28 my-11'>
+                <div>
                     <img className='h-[180px] w-48 rounded-full' src={choose} alt="" />
-                    <div className=''>
-                        <h3 className='text-accent text-3xl font-bold font-serif  '>Choose A Toy</h3>
+                    <div className='my-6'>
+                        <h3 className='text-accent text-3xl font-bold font-serif my-2 '>Choose A Toy</h3>
                         <p className='font-serif'>pick from over 1000 of the <br /> latest must haves</p>
                     </div>
                 </div>
-                <div>
-                    <img className='h-[180px] w-48' src={plan} alt="" />
-                    <div className=''>
-                        <h3 className='text-accent text-3xl font-bold font-serif  '>Plan and Learn</h3>
+                <div >
+                    <img className='h-[180px] w-48 rounded-full' src={plan} alt="" />
+                    <div className='my-6'>
+                        <h3 className='text-accent text-3xl font-bold font-serif my-2 '>Plan and Learn</h3>
                         <p className='font-serif'>Kid-led Discovery that builds <br /> confidens and sparks magination</p>
                     </div>
                 </div>
 
                 <div>
-                    <img className='h-[180px] w-48' src={swap} alt="" />
-                    <div className=''>
-                        <h3 className='text-accent text-3xl font-bold font-serif  '>Swap and Repeat</h3>
-                        <p className='font-serif'>Return any unloved toy and swap it for somting else</p>
+                    <img className='h-[180px] w-48 rounded-full' src={swap} alt="" />
+                    <div className='my-6'>
+                        <h3 className='text-accent text-3xl font-bold font-serif my-2  '>Swap and Repeat</h3>
+                        <p className='font-serif'>Return any unloved toy and <br /> swap it for somting else</p>
                     </div>
                 </div>
-                
+
             </div>
+            <div className='text-center mb-8'>
+                <button className="btn btn-lg gap-2 bg-accent text-white font-bold border-none ">
+                    Go To Buy
+                    <FaArrowCircleRight />
+
+                </button>
             </div>
+        </div>
 
 
 
 
-            );
+    );
 };
 
-            export default ExtraFirst;
+export default ExtraFirst;
